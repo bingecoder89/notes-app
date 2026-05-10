f4ced49d29e2cbbc44bbc5ab3e11992cbdf385d6
Ankit Bal <bingecoder89@gmail.com>
2026-05-10 15:25:47 +0530

    feat: add next-themes and wrap App component in ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div>App</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
